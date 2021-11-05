9e5e96b8b4a1201fc23265eab3b0c0f1beb6a291
Francesco <scovascova@gmail.com>
2021-11-05 16:56:06 +0100

    if NaN per l'età

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ console.log('nome:'+ chilometri);
 
 let eta = prompt('Quanti chilometri vuoi percorrere?');
 console.log('nome:'+ eta);
+
+if (isNaN(eta)){
+    console.log('Non hai inserito un numero')
+}
