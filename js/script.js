7b7ea15c092de20e4b6bb1a04181992609fd0187
Francesco <scovascova@gmail.com>
2021-11-05 17:13:56 +0100

    prezzo della tratta

--- js/script.js
+++ js/script.js
@@ -16,3 +16,15 @@ console.log('età:'+ eta);
 if (isNaN(eta)){
     console.log('Non hai inserito un numero')
 }
+
+
+// prezzo della tratta
+let prezzoTratta = chilometri * 0.21;
+
+if (eta < 18) {
+    console.log (prezzoTratta - prezzoTratta * 20 / 100);
+  } else if (eta > 65) {
+    console.log(prezzoTratta - prezzoTratta * 40 / 100);
+  } else {
+    console.log (prezzoTratta);
+  }
