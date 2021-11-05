6a73d515d6795c55d232f0930a9c0ea387245355
Francesco <scovascova@gmail.com>
2021-11-05 16:47:16 +0100

    const

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// alert('ciao');
+
+
+const chilometri = prompt('Quanti chilometri vuoi percorrere?');
+console.log('nome:'+ chilometri);
+
+const eta = prompt('Quanti chilometri vuoi percorrere?');
+console.log('nome:'+ chilometri);
